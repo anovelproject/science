1792b0208c2afc66ff9646c5bb04aa4970a85d06
Erin Zhang <zyyerin@gmail.com>
2018-03-01 12:01:02 -0800

    sticky header

---
+++ main.js
@@ -0,0 +1,20 @@
+$(function(){
+
+  $(document).scroll(function() {
+      let dist = $(document).scrollTop();
+      let alpha = (1 - dist/70).clamp(0, 1);
+      let alpha2 = (dist/70-1).clamp(0, 1);
+      $("#lockup").css("opacity", alpha);
+      $("#lockup2").css("opacity", alpha2);
+
+
+      console.log(alpha);
+
+  });
+
+});
+
+
+Number.prototype.clamp = function(min, max) {
+  return Math.min(Math.max(this, min), max);
+};
